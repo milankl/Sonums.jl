d2169dcc44868c2841534ba807c81c309b34e57a
milankl <milankloewer@gmx.de>
2019-07-02 13:17:58 +0100

    Int promotion

--- src/conversionsInt.jl
+++ src/conversionsInt.jl
@@ -3,3 +3,12 @@ Optim16(x::UInt16) = reinterpret(Optim16,x)
 
 UInt8(x::Optim8) = reinterpret(UInt8,x)
 UInt16(x::Optim16) = reinterpret(UInt16,x)
+
+Optim8(x::Int) = Optim8(Float64(x))
+Optim16(x::Int) = Optim16(Float64(x))
+
+promote_rule(::Type{Int64},::Type{Optim16}) = Optim16
+promote_rule(::Type{Int64},::Type{Optim8}) = Optim8
+
+promote_rule(::Type{Int32},::Type{Optim16}) = Optim16
+promote_rule(::Type{Int32},::Type{Optim8}) = Optim8
