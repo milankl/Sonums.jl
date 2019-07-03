b09cd84d86dad8c463086571e842183b7bff368b
milankl <milankloewer@gmx.de>
2019-07-03 18:42:49 +0100

    Integer conversion

--- src/conversionsInt.jl
+++ src/conversionsInt.jl
@@ -4,6 +4,9 @@ Optim16(x::UInt16) = reinterpret(Optim16,x)
 UInt8(x::Optim8) = reinterpret(UInt8,x)
 UInt16(x::Optim16) = reinterpret(UInt16,x)
 
+Int(x::Optim8) = Int(UInt8(x))
+Int(x::Optim16) = Int(UInt16(x))
+
 Optim8(x::Int) = Optim8(Float64(x))
 Optim16(x::Int) = Optim16(Float64(x))
 
