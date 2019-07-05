b206036f9e779db63c11e29edba08a19254cfef7
milankl <milankloewer@gmx.de>
2019-07-05 14:13:27 +0100

    Conversion and arithmetic tests included

--- src/round.jl
+++ src/round.jl
@@ -1,7 +1,7 @@
 function round2optim8(x::Float64)
-    return signbit(x) ? -optim8[argmin(-x >= bounds8)] : optim8[argmin(x >= bounds8)]
+    return signbit(x) ? -optim8[findFirstSmaller(-x,bounds8)-2)] : optim8[findFirstSmaller(x,bounds8)-2)]
 end
 
 function round2optim16(x::Float64)
-    return signbit(x) ? -optim16[argmin(-x >= bounds16)] : optim16[argmin(x >= bounds16)]
+    return signbit(x) ? -optim16[findFirstSmaller(-x,bounds16)-2)] : optim16[findFirstSmaller(-x,bounds16)-2)]
 end
---
+++ test/test_conversions.jl
@@ -0,0 +1,17 @@
+@testset "Conversions" begin
+
+    @testset for T in (Optim8, Optim16)
+        @test one(T) == T(1.0)
+        @test zero(T) == T(0.0)
+        @test minusone(T) == T(-1.0)
+        @test notareal(T) == T(Inf)
+
+        for x in [-Inf,-1.0,-0.25,0.0,0.25,1.0,Inf]
+            @test T(x) == T(Float64(T(x)))
+        end
+
+        @test floatmin(T) == T(Float64(floatmin(T)))
+        @test floatmax(T) == T(Float64(floatmax(T)))
+
+    end
+end
