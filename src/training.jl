c4381360eaad907fdc3f3aefe501028f3065677a
milankl <milankloewer@gmx.de>
2019-07-11 18:47:37 +0100

    setSonum8,16 functions

--- src/training.jl
+++ src/training.jl
@@ -37,6 +37,34 @@ function trainSonum(nbit::Int,data::Union{AbstractArray{Float32},AbstractArray{F
     calcBounds(nbit,sonum)
 end
 
+function setSonum8(nums::Union{AbstractArray{Float32,1},AbstractArray{Float64,1}})
+    setSonum(8,nums)
+end
+
+function setSonum16(nums::Union{AbstractArray{Float32,1},AbstractArray{Float64,1}})
+    setSonum(16,nums)
+end
+
+function setSonum(nbit::Int,nums::Union{AbstractArray{Float32,1},AbstractArray{Float64,1}})
+
+    # for 8bit, vector nums has to be 127 elements long, excluding 0 and Inf
+    # for 16bit, vector nums has to be 127 elements long, excluding 0 and Inf
+
+    if nbit == 8 && length(nums) == 2^7-1
+        sonum = sonum8
+    elseif nbit == 16 && lengt(nums) == 2^15-1
+        sonum = sonum16
+    else
+        throw(error("Only 8/16bit supported."))
+    end
+
+    sonum[1] = 0.0
+    sonum[end] = Inf
+    sonum[2:end-1] = nums
+
+    calcBounds(nbit,sonum)
+end
+
 function calcBounds(nbit::Int,numlist::Array{Float64,1})
 
     numlist[1] == 0 || throw(error("The first element of x should be 0."))
